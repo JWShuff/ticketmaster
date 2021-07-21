7b5248738d8694365bde3ad5659c39cc3ab5dcab
jwshuff <16825267+JWShuff@users.noreply.github.com>
2021-07-21 16:36:27 -0400

    Google Embed up and working.

--- ticket/views.py
+++ ticket/views.py
@@ -2,8 +2,12 @@ from django.conf import settings
 from django.shortcuts import render,redirect,HttpResponse
 
 def home(request):
-    return HttpResponse(_generate_google_address('Space+Needle,Seattle+WA'))
+    return render(request, 'home.html')
 
 def _generate_google_address(address):
     modified_address = address.replace(' ', '%20')
     return f"https://www.google.com/maps/embed/v1/place?q={modified_address}&key={settings.MAPS_EMBED_KEY}"
+
+def _generate_ticket_artist_search(artist):
+    ticket_artist_search=artist
+    return f"https://developer.ticketmaster.com/products-and-docs/apis/discovery-api/v2/"
